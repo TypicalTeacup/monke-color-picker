78e933f56de6ed8e63de40efc930d275396503d8
TypicalTeacup <pskirel@wp.pl>
2022-02-01 10:43:41 +0100

    looks and stuff

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
+    	r: parseInt(result[1], 16),
+    	g: parseInt(result[2], 16),
+    	b: parseInt(result[3], 16)
     } : null;
 }
 
 function rgbToMonke(r,g,b) {
     return {
-      r: Math.round(r/28.333),
-      g: Math.round(g/28.333),
-      b: Math.round(b/28.333)
+    	r: Math.round(r/28.333),
+    	g: Math.round(g/28.333),
+    	b: Math.round(b/28.333)
     };
 }
 
 function monkeToRgb(monkeR, monkeG, monkeB){
     return {
-        r: clamp(Math.floor(monkeR*28.333),0,255),
-        g: clamp(Math.floor(monkeG*28.333),0,255),
-        b: clamp(Math.floor(monkeB*28.333),0,255)
-      };
+        r: Math.round(monkeR*28.333),
+        g: Math.round(monkeG*28.333),
+        b: Math.round(monkeB*28.333)
+    };
+}
+
+function peepeepoopoo(ok,ok2){
+	document.getElementById(ok2[0]).innerHTML = ok.r;
+    document.getElementById(ok2[1]).innerHTML = ok.g;
+    document.getElementById(ok2[2]).innerHTML = ok.b;
 }
 
 window.onload=function(){
-    let colorInput = document.getElementById('color');
+    let colorInput = document.getElementById('color-input');
 
     colorInput.addEventListener('input', () =>{
         let inputRgb = hexToRgb(colorInput.value);
-        let monke = rgbToMonke(inputRgb.r,inputRgb.g,inputRgb.b);
         
-        document.getElementById("monke").innerHTML = "Monke color: " + monke.r + ", " + monke.g + ", " + monke.b;
+		let monke = rgbToMonke(inputRgb.r,inputRgb.g,inputRgb.b);
+        peepeepoopoo(monke,["monkeR","monkeG","monkeB"])
+
         let rgbResult = monkeToRgb(monke.r, monke.g, monke.b);
-        console.table(inputRgb);console.table(rgbResult);console.table(monke);
-        document.getElementById("rgb").innerHTML = "RGB equivalent: " + rgbResult.r + ", " + rgbResult.g + ", " + rgbResult.b;
+		peepeepoopoo(rgbResult,["r","g","b"])
+		document.getElementById("results").style = "";
+		document.getElementById("prompt").style = "opacity: 0;";
     });
 }
